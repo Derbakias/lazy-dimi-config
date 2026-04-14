117fd8cc5494f7db5131d2a68659f896de36deb8
Derbakias <dimitrios.danousis@gmail.com>
2026-04-14 10:39:02 +0100

    Disabled scroll EOF in anything else apart from the file buffers

--- lua/plugins/scrolleof.lua
+++ lua/plugins/scrolleof.lua
@@ -3,5 +3,14 @@ return {
     "Aasim-A/scrollEOF.nvim",
     event = { "CursorMoved", "WinScrolled" },
     opts = {},
+    config = function(_, opts)
+      local scrollEOF = require("scrollEOF")
+      scrollEOF.setup(opts)
+      setmetatable(scrollEOF.opts.disabled_filetypes, {
+        __index = function()
+          return vim.bo.buftype ~= ""
+        end,
+      })
+    end,
   },
 }
